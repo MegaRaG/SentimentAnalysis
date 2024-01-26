668754c98e9fee84cc082af576f2356a4bd4b5d7
Ahmed-Omer <Ahmed.Omerovic@etu.univ-savoie.fr>
2024-01-26 10:25:57 +0100

    Initial Commit

---
+++ SentimentAnalysis/SentimentAnalysisApp.swift
@@ -0,0 +1,17 @@
+//
+//  SentimentAnalysisApp.swift
+//  SentimentAnalysis
+//
+//  Created by Ahmed OMEROVIC on 1/26/24.
+//
+
+import SwiftUI
+
+@main
+struct SentimentAnalysisApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
